23d37c12eebe736616641ab825a8580343e02a92
Nataliy80 <nataliamalinkina80gmail.com>
2022-08-21 23:58:58 +0300

    Добавлен код для меню бургер

--- JS/script.js
+++ JS/script.js
@@ -7,6 +7,7 @@ if (iconMenu) {
         iconMenu.classList.toggle('_active');
         menuBody.classList.toggle('_active');
     })
+
 }
 
 // Прокрутка при клике
@@ -15,13 +16,26 @@ if (menuLinks.length > 0) {
     menuLinks.forEach(menuLink => {
         menuLink.addEventListener("click", onMenuLinkClick);
     });
-}
 
-function onMenuLinkClick(e) {
-    const menuLink = e.target;
-    if (iconMenu.classList.contains('_active')) {
-        document.body.classList.remove('_lock');
-        iconMenu.classList.remove('_active');
-        menuBody.classList.remove('_active');
+
+    function onMenuLinkClick(e) {
+        const menuLink = e.target;
+        if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+            const gotoBlock = document.querySelector(menuLink.dataset.goto);
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+
+            window.scrollTo({
+                top: gotoBlockValue,
+                behavior: "smooth"
+            });
+            e.preventDefault();
+        }
+
+        if (iconMenu.classList.contains('_active')) {
+            document.body.classList.remove('_lock');
+            iconMenu.classList.remove('_active');
+            menuBody.classList.remove('_active');
+        }
     }
 }
+
